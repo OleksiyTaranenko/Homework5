b73cc6d93666fceca8bd2f0f508e3aaa1dd6a1b8
OleksiyTaranenko <77008282+OleksiyTaranenko@users.noreply.github.com>
2023-07-25 14:51:44 +0300

    Update task2-1.js

--- js/task2-1.js
+++ js/task2-1.js
@@ -1,29 +1,18 @@
-let sequence2 = [1, 1000];
-sequence2.length = 1000;
-
-function showNumbers(number) {    
-    for (let i = 0; i <= 1000; i++) {
+function showNumbers2(number) {
+    let sequence2 = [];    
+    
+    for (let i = 1; i <= 1000; i++) {
         sequence2[i] = i;
     }
     
-    sequence2.splice(0, 1);
-        
-    let filteredNumbers1 = sequence2.filter((element) => {
-        return element % 2 === 0;            
-    });
+    let filteredNumbers1 = sequence2.filter((element) => {        
+        return element % 2 === 0 && element % 10 === 0;              
+    });    
     
-    let filteredNumbers2 = sequence2.filter((element) => {
-         return element % 10 === 0;            
-    });
-
-    if (number >= 1000) {
-        console.log(filteredNumbers1);
+    if (number >= 0) {        
         return filteredNumbers1;
-    } else {
-        console.log(filteredNumbers2);
-         return filteredNumbers2;
-        }
-    }    
+    }         
+}    
     
-showNumbers(1000);  
-showNumbers(999);  
+let result2 = showNumbers2(1000);
+console.log(result2);
